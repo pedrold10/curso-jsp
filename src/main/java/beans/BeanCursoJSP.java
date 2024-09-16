4847a9eac4f8cd123ae2ed7aaa49b50f3ba3c3c6
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2024-09-16 17:34:01 -0300

    Implementando getters e setters

--- src/main/java/beans/BeanCursoJSP.java
+++ src/main/java/beans/BeanCursoJSP.java
@@ -1,8 +1,32 @@
 package beans;
 
 public class BeanCursoJSP{
+	private String nome;
+	private String ano;
+	private String sexo;
 	
-	public int calcula(int numero) {
-		return numero * 100;
+	public String getNome() {
+		return this.nome;
 	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getAno() {
+		return this.ano;
+	}
+	
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+	
+	public String getSexo() {
+		return this.sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
 }
